bf742d651b566e47c08c8777f02b1b169762335b
Karthik A <karthikthee7@gmail.com>
2020-09-04 17:04:50 +0530

    Add Theme File

---
+++ client/src/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+  },
+});
