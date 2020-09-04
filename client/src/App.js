e3f666f25f8c7c06afa1e425a22c13aa76874937
Karthik A <karthikthee7@gmail.com>
2020-09-04 17:09:15 +0530

    It will Change

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import "./App.css";
+
+import { ThemeProvider } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+import { theme } from "./theme";
+
+import { useStyles } from "./AppStyle";
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <h1>இராவணன் கலைகூடம்</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <h1 className={classes.title}>இராவணன் கலைகூடம்</h1>
+      <Button variant="contained" color="primary">
+        Hi
+      </Button>
+    </ThemeProvider>
   );
 }
 
