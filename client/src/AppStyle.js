65f8736a4b97f1ca2a213345599dacedc246bb7a
Karthik A <karthikthee7@gmail.com>
2020-09-04 17:05:57 +0530

    File name changed

---
+++ client/src/AppStyle.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { green } from "@material-ui/core/colors";
+
+export const useStyles = makeStyles({
+  title: {
+    fontWeight: 400,
+    color: green[600],
+  },
+});
