6a3229bc530dc65fb00c06be491cd36a2a01b104
Karthik A <karthikthee7@gmail.com>
2020-09-19 10:26:21 +0530

    theme file not working

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,7 +1,7 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 
-const theme = createMuiTheme({
+export const dark = createMuiTheme({
   palette: {
     type: 'dark',
     primary: {
@@ -10,4 +10,4 @@ const theme = createMuiTheme({
   },
 });
 
-export default theme;
+export const light = createMuiTheme({});
