3390039a87ff1c0bea627585f6508585626c9e9d
Karthik A <karthikthee7@gmail.com>
2020-09-24 18:19:31 +0530

    Setup Rest_Framework

--- server/raavanan/settings.py
+++ server/raavanan/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -99,6 +102,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+# REST_FRAMEWORK
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
