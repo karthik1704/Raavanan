129559bf7f1d9a4a2aaac60b818d0dc8060fbfdd
Karthik A <karthikthee7@gmail.com>
2020-10-13 12:18:33 +0530

    trying product

--- server/products/models.py
+++ server/products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProductCategory(models.Model):
+    category_name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.category_name
+
+class SubCategory(models.Model):
+    category_id = models.ForeignKey(ProductCategory,on_delete=models.CASCADE)
+    SubCategory_Name = models.CharField(max_length=30)  
+
+    def __str__(self) -> str:
+        return self.SubCategory_Name
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200)
+    category_id = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.product_name
--- server/raavanan/urls.py
+++ server/raavanan/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('products/',include('produts.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
