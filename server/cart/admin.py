f1776a441af70f298987323cb42511e65a7b028e
Karthik A <karthikthee7@gmail.com>
2020-10-27 20:30:21 +0530

    Add Cart Model

--- server/cart/admin.py
+++ server/cart/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Cart , CartItem
+
 # Register your models here.
+class CartItemAdmin(admin.StackedInline):
+    model = CartItem
+
+class CartAdmin(admin.ModelAdmin):
+    inlines = (CartItemAdmin,)
+
+
+admin.site.register(Cart, CartAdmin)
--- server/cart/models.py
+++ server/cart/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from products.models import Product
+
+from django.contrib.auth import get_user_model
+
 # Create your models here.
+User = get_user_model()
+
+class Cart(models.Model):
+    user = models.ForeignKey(User,on_delete= models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    total_price = models.FloatField(blank=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+
+    
