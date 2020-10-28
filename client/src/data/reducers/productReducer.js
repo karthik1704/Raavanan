652bcbeca159ef8d49cc52b2860873d6150c00f7
Karthik A <karthikthee7@gmail.com>
2020-10-28 20:38:56 +0530

    Removing Hard Code

--- client/src/data/reducers/productReducer.js
+++ client/src/data/reducers/productReducer.js
@@ -1,66 +1,7 @@
 import { FETCH_PRODUCT_DETAIL, FETCH_PRODUCT } from '../CONSTANTS';
 
-import Image1 from '../../asserts/images/main model 3.png';
-import Image2 from '../../asserts/images/main model 4.png';
-import Image3 from '../../asserts/images/main model 5.png';
-
 const initalState = {
-  products: [
-    // {
-    //   id: 1,
-    //   name: 'Ma Phone Case 1',
-    //   price: 399,
-    //   offer: '20%',
-    //   category: 'phonecase',
-    //   mrp: 500,
-    //   imageUrl: Image1,
-    // },
-    // {
-    //   id: 2,
-    //   name: 'சேயோன் கைபேசி உறை',
-    //   price: 399,
-    //   offer: '20%',
-    //   category: 'phonecase',
-    //   mrp: 500,
-    //   imageUrl: Image2,
-    // },
-    // {
-    //   id: 3,
-    //   name: 'Phone Case 3',
-    //   price: 399,
-    //   offer: '20%',
-    //   category: 'phonecase',
-    //   mrp: 500,
-    //   imageUrl: Image3,
-    // },
-    // {
-    //   id: 4,
-    //   name: 'Phone Case 1',
-    //   price: 399,
-    //   offer: '20%',
-    //   category: 'phonecase',
-    //   mrp: 500,
-    //   imageUrl: Image1,
-    // },
-    // {
-    //   id: 5,
-    //   name: 'Phone Case 2',
-    //   price: 399,
-    //   offer: '20%',
-    //   category: 'phonecase',
-    //   mrp: 500,
-    //   imageUrl: Image2,
-    // },
-    // {
-    //   id: 6,
-    //   name: 'Phone Case 3',
-    //   price: 399,
-    //   offer: '20%',
-    //   category: 'phonecase',
-    //   mrp: 500,
-    //   imageUrl: Image3,
-    // },
-  ],
+  products: [],
   productDetail: {},
 };
 
@@ -70,7 +11,6 @@ const productReducer = (state = initalState, { type, payload }) => {
       return {
         ...state,
         products: payload,
-      
       };
     case FETCH_PRODUCT_DETAIL:
       return {
