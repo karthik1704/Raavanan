cc22cfdc562b68bc5180fd522ca585e778d11b66
Karthik A <karthikthee7@gmail.com>
2020-10-28 18:51:09 +0530

    Add orders to Admin Panel

--- server/orders/admin.py
+++ server/orders/admin.py
@@ -1,18 +1,14 @@
 from django.contrib import admin
 
-from .models import Order, OrderItem, OrderItemExtra
+from .models import Order, OrderItem
 
 # Register your models here.
 
-class OrderItemExtraAdmin(admin.StackedInline):
-    model = OrderItemExtra
     
 
 class OrderItemAdmin(admin.StackedInline):
     model = OrderItem
-    inlines = (OrderItemExtraAdmin,)
-
-admin.site.register(OrderItem , OrderItemAdmin)
+ 
 
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderItemAdmin,)
