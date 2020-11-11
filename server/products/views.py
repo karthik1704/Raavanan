021cf9d6d688dd2ad4b89386b9f9b617d19aad38
Karthik A <karthikthee7@gmail.com>
2020-11-11 19:01:18 +0530

    add serializer changes

--- server/products/views.py
+++ server/products/views.py
@@ -1,5 +1,6 @@
 from django.db.models import fields
 from django.shortcuts import render
+from rest_framework.relations import PrimaryKeyRelatedField
 
 from .models import Product, ProductImage, ProductMaterial , Category
 
@@ -10,13 +11,18 @@ from rest_framework import viewsets, serializers
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImage
-        fields = ['product_id','image']
+        fields = ['id','image']
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = PrimaryKeyRelatedField(queryset=Category.objects.all())
+    #category = serializers.SlugRelatedField(many=True, read_only=True, slug_field='slug')
+    images = ProductImageSerializer(many=True, read_only=True)
     class Meta:
         model= Product
-        fields=['id','product_name', 'brand','manufacturer','imageurl', 'supported_devices','origin','description']
+        fields=['id','product_name', 'brand','manufacturer','imageurl', 'category', 'supported_devices','origin','description', 'images']
+
+
 class ProductViewset(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
