1e2283d928306454d1781164b0c2b67fd017d1cc
Karthik A <karthikthee7@gmail.com>
2020-11-11 19:59:37 +0530

    Add Materials and Category to serialaizer

--- server/products/views.py
+++ server/products/views.py
@@ -1,6 +1,6 @@
 from django.db.models import fields
 from django.shortcuts import render
-from rest_framework.relations import PrimaryKeyRelatedField
+from rest_framework.relations import PrimaryKeyRelatedField, StringRelatedField
 
 from .models import Product, ProductImage, ProductMaterial , Category
 
@@ -8,19 +8,32 @@ from rest_framework import viewsets, serializers
 
 # Create your views here.
 
+
+class CategorySerializer(serializers.ModelSerializer):
+    parent_category = StringRelatedField()
+    class Meta:
+        model = Category
+        fields = ['id','category_name', 'parent_category']
+
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImage
         fields = ['id','image']
 
 
+class ProductMaterialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductMaterial
+        fields = ['id','material_name']
+
 class ProductSerializer(serializers.ModelSerializer):
-    category = PrimaryKeyRelatedField(queryset=Category.objects.all())
+    category = CategorySerializer()
     #category = serializers.SlugRelatedField(many=True, read_only=True, slug_field='slug')
-    images = ProductImageSerializer(many=True, read_only=True)
+    #images = PrimaryKeyRelatedField()
+    materials = ProductMaterialSerializer(many=True)
     class Meta:
         model= Product
-        fields=['id','product_name', 'brand','manufacturer','imageurl', 'category', 'supported_devices','origin','description', 'images']
+        fields=['id','product_name', 'brand','manufacturer','imageurl', 'category', 'materials', 'supported_devices','origin','description']
 
 
 class ProductViewset(viewsets.ModelViewSet):
