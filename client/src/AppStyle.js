22ebb7acf83b508a5fdaccfbd31c00c5600de97d
Karthik A <karthikthee7@gmail.com>
2020-11-14 16:40:19 +0530

    Add Min height 100 Vh

--- client/src/AppStyle.js
+++ client/src/AppStyle.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: '53vh',
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.down('sm')]: {
       minHeight: '100vh',
     },
   },
