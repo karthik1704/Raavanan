5accc8edbb81d95634630c5d077d28dd55a34952
Karthik A <karthikthee7@gmail.com>
2020-11-14 14:07:51 +0530

    Small Changes

--- client/src/AppStyle.js
+++ client/src/AppStyle.js
@@ -1,5 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const useStyles = makeStyles({
-  root: {},
-});
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '53vh',
+    [theme.breakpoints.up('sm')]: {
+      minHeight: '100vh',
+    },
+  },
+}));
