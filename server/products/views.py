17ed06b2f87428be21dcd79130a01ebd42aee125
Karthik A <karthikthee7@gmail.com>
2020-11-15 12:21:46 +0530

    Add filters

--- server/products/views.py
+++ server/products/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 
@@ -9,8 +10,9 @@ from .models import Category, Product
 class ProductViewset(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['category__slug', 'category__parent_category' ]
+    search_fields = ['product_name','category__category_name']
     ordering_fields = ['created_at']
     ordering = ['-created_at']
 
