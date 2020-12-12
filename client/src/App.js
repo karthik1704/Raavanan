bacdd53c8132a0e8f5aab9055f5b94676aed5d01
Karthik A <karthikthee7@gmail.com>
2020-12-12 18:10:57 +0530

    Add React GA

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
@@ -16,7 +17,12 @@ import Footer from './components/Footer/Footer';
 
 // import { dark, light } from './theme';
 
+import ReactGA from 'react-ga';
+import { Helmet } from 'react-helmet';
+
 import { useStyles } from './AppStyle';
+
+import Logo from './asserts/raavanan logo.png';
 
 function App() {
   const classes = useStyles();
@@ -37,19 +43,39 @@ function App() {
     },
   });
 
+  useEffect(() => {
+    ReactGA.initialize('G-LH9KB8TXPW');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <Router>
-        <Paper>
-          {loading && <Loader />}
-          <Navbar />
-          <Container className={classes.root}>
-            <Routes />
-          </Container>
-          <Footer />
-        </Paper>
-      </Router>
-    </ThemeProvider>
+    <>
+      <Helmet>
+        <title>இராவணன் அங்காடி | முகப்பு</title>
+        <meta
+          name="keywords"
+          content="ravanan,raavanan, ravanan store, raavanan store, phone case,photo frame,mugs,cups,stickers,tamil Dhesiyam, ntk, naam tzhamilar, seeman,valluvar, raavananstore "
+        />
+        <meta property="og:title" content="Raavanan Store , இராவணன் அங்காடி" />
+        <meta property="og:image" content={Logo} />
+        <meta
+          property="og:description"
+          content="Raavanan Store , இராவணன் அங்காடி"
+        />
+      </Helmet>
+      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        <Router>
+          <Paper>
+            {loading && <Loader />}
+            <Navbar />
+            <Container className={classes.root}>
+              <Routes />
+            </Container>
+            <Footer />
+          </Paper>
+        </Router>
+      </ThemeProvider>
+    </>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,12 @@ import './index.css';
 import { Provider } from 'react-redux';
 import store from './data/store';
 
+import ReactGA from 'react-ga';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+ReactGA.initialize('G-LH9KB8TXPW');
 
 ReactDOM.render(
   <StrictMode>
