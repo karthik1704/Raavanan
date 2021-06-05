ceb21ee0a95daaaed02e09166d84a7357a44b6d0
Karthik A <karthikthee7@gmail.com>
2021-06-05 20:52:49 +0530

    production changes

--- client/src/CONSTANTS.js
+++ client/src/CONSTANTS.js
@@ -1,6 +1,6 @@
-// export const API_URL = 'https://api.raavananstore.com/';
-//export const API_URL = 'http://api.raavananstore.com/';
-export const API_URL = 'http://127.0.0.1:8000/';
+export const API_URL = 'https://api.raavananstore.com/';
+// export const API_URL = 'http://api.raavananstore.com/';
+// export const API_URL = 'http://127.0.0.1:8000/';
 // export const API_URL = 'http://192.168.1.11:8000/';
 
 // Test Commit
