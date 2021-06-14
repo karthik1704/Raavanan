f7207d4e2ad419e236d4b1bc22c29f6b5fdc5cbc
Krishna sethupathy <30381844+krishnasethupathy-s@users.noreply.github.com>
2021-06-15 00:28:39 +0530

    constatnt for paytm

--- client/src/CONSTANTS.js
+++ client/src/CONSTANTS.js
@@ -1,6 +1,10 @@
 // export const API_URL = 'https://api.raavananstore.com/';
 //export const API_URL = 'http://api.raavananstore.com/';
 export const API_URL = 'http://127.0.0.1:8000/';
+// export const PAYTM_MID = 'GyzTqR94486284090859'; //Production
+export const PAYTM_MID = 'BapaHR15076620391870'; //Dev
+// export const ENV = 'PROD'
+export const ENV = 'ENV'
 // export const API_URL = 'http://192.168.1.11:8000/';
 
 // Test Commit
