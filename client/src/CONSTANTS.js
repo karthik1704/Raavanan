59601573ad635efca0aab000c2fcead5dfe8a8d0
Krishna sethupathy <30381844+krishnasethupathy-s@users.noreply.github.com>
2021-07-02 22:40:30 +0530

    Add files via upload

---
+++ client/src/CONSTANTS.js
@@ -0,0 +1,53 @@
+// export const API_URL = 'https://api.raavananstore.com/';
+//export const API_URL = 'http://api.raavananstore.com/';
+export const API_URL = 'http://127.0.0.1:8000/';
+// export const PAYTM_MID = 'GyzTqR94486284090859'; //Production
+export const PAYTM_MID = 'BapaHR15076620391870'; //Dev
+// export const ENV = 'PROD'
+export const ENV = 'ENV'
+// export const API_URL = 'http://192.168.1.11:8000/';
+
+// Test Commit
+
+export const MENUS = [
+    {
+        'menu' : 'இயற்கை உணவு',
+        'link' : 'organic-foods',
+        
+    },
+    {
+        'menu' : 'கைபேசி உறை',
+        'link' : 'phone-cases',
+        'submenu' : [
+            {
+                'menu' : 'தமிழ் தேசியம்',
+                'link' : 'tamil-desiyam-cases',
+            },
+            {
+                'menu' : 'தலைவர் பிரபாகரன்',
+                'link' : 'leader-prabhakaran-cases',
+            }
+        ]
+    },
+    {
+        'menu' : 'படச்சட்டகம்',
+        'link' : 'photo-frames',
+        'link' : 'phone-cases',
+        'submenu' : [
+            {
+                'menu' : 'தமிழ் தேசியம்',
+                'link' : 'tamil-desiyam-photoframes',
+            },
+            {
+                'menu' : 'தலைவர் பிரபாகரன்',
+                'link' : 'leader-prabhakaran-photoframes',
+            }
+        ]
+        
+    },
+    {
+        'menu' : 'கொசுவு சட்டைகள்',
+        'link' : 't-shirts',
+        
+    }
+]
