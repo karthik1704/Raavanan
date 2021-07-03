bec3d6587111f264fbda386e3a0571e51d860d37
Karthik A <karthikthee7@gmail.com>
2021-07-03 20:37:48 +0530

    Carousel update

--- client/src/components/carousel/Carousel.js
+++ client/src/components/carousel/Carousel.js
@@ -1,10 +1,11 @@
-import Card from '@material-ui/core/Card';
+import { Slide } from 'react-slideshow-image';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-import CarouselImage1 from '../../asserts/images/3.jpg';
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css'
+import CarouselImage1 from '../../asserts/images/banner1.jpg';
+import CarouselImage2 from '../../asserts/images/banner2.jpg';
+import CarouselImage3 from '../../asserts/images/banner3.jpg';
+import 'react-slideshow-image/dist/styles.css';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,31 +29,26 @@ const useStyles = makeStyles((theme) => ({
 
 const Carousel = () => {
   const classes = useStyles();
-  const slideImages = [CarouselImage1]
+  const slideImages = [CarouselImage1, CarouselImage2, CarouselImage3];
   return (
-    // <Card className={classes.root}>
-    //   <img src={CarouselImage1} className={classes.media} alt="image1" />
-    // </Card>
-    
-      <div className="slide-container">
-        <Slide>
-          {
-          slideImages.map((image) => (
-
-                        
-            <div className="each-slide">
-              <div className={classes.media} style={{'height': '200px','width': '100%','backgroundSize': 'cover',
-    'backgroundRepeat': 'no-repeat','backgroundImage': `url(${image})`}}>
-                {/* <span>Slide 1</span> */}
-              </div>
-            </div>
-          ))
-          }
-        
-         
-        </Slide>
-      </div>
-    
+    <div className="slide-container">
+      <Slide>
+        {slideImages.map((image, idx) => (
+          <div className="each-slide" key={idx}>
+            <div
+              style={{
+                backgroundImage: `url(${image})`,
+                height: '260px',
+                width: '100%',
+                backgroundSize: '100% 220px',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+              }}
+            ></div>
+          </div>
+        ))}
+      </Slide>
+    </div>
   );
 };
 
