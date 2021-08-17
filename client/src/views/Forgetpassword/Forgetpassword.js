279b1b3b82ac04323de12a912ea86d4de01a9c7f
Karthik A <karthikthee7@gmail.com>
2021-08-17 20:47:11 +0530

    removed unused imports

--- client/src/views/Forgetpassword/Forgetpassword.js
+++ client/src/views/Forgetpassword/Forgetpassword.js
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-//import { useSelector, useDispatch } from 'react-redux';
-import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/core/Alert';
+import Snackbar from '@material-ui/core/Snackbar';
+import Typography from '@material-ui/core/Typography';
+
+//import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import MuiPhoneInput from 'material-ui-phone-number';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
-import { useState, useEffect } from 'react';
-import Box from '@material-ui/core/Box';
+
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import makeStyles from '@material-ui/styles/makeStyles';
+
 import Container from '@material-ui/core/Container';
 import Header from '../../components/Header/Header';
 import { API_URL } from '../../CONSTANTS';
