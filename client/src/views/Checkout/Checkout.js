370177c1dacdb3ec81b9908ef87aad273964c298
Karthik A <karthikthee7@gmail.com>
2021-08-17 20:39:57 +0530

    removed unused imports and style not fixed

--- client/src/views/Checkout/Checkout.js
+++ client/src/views/Checkout/Checkout.js
@@ -1,19 +1,10 @@
 import { useState, useEffect } from 'react';
-import { useParams, useLocation } from 'react-router-dom';
 import React from 'react';
-import axois from 'axios';
+
 import Snackbar from '@material-ui/core/Snackbar';
-import clsx from 'clsx';
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
-import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-import WhatsAppIcon from '@material-ui/icons/WhatsApp';
 import axios from 'axios';
 import MuiAlert from '@material-ui/core/Alert';
 import Accordion from '@material-ui/core/Accordion';
@@ -24,21 +15,16 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { useSelector, useDispatch } from 'react-redux';
 import makeStyles from '@material-ui/styles/makeStyles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Paper from '@material-ui/core/Paper';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
-import Link from '@material-ui/core/Link';
-import Cartpage from '../../components/Checkout/Cartpage';
 
+import Cartpage from '../../components/Checkout/Cartpage';
 import AddressForm from '../../components/Checkout/AddressForm';
 import PaymentForm from '../../components/Checkout/PaymentForm';
-import Review from '../../components/Checkout/Review';
+// import Review from '../../components/Checkout/Review';
 import './Checkout.css';
-
 
 import { API_URL } from '../../CONSTANTS';
 
@@ -147,85 +133,83 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const steps = ['Address', 'Review your order', 'Payment', ];
+const steps = ['Address', 'Review your order', 'Payment'];
 
-const Addresses =(props) => {
-  console.log(props)
- 
+const Addresses = (props) => {
+  console.log(props);
 
-  
   // props.addresses.map((address) => {
   //   // obj[address.id] = false
   //   address['checked'] = false;
-    
-  // })
-  
 
-  console.log(props.addresses)
-  
+  // })
+
+  console.log(props.addresses);
+
   const checked = props.checked;
-  const setChecked = props.setChecked
+  const setChecked = props.setChecked;
   // console.log('state')
   // console.log(state1);
 
   const handleCheck = (event) => {
-    const value  = event.target.name;
-    
-    setChecked(checked.includes(value) ? checked.filter(c => c !== value) : [value]);    
+    const value = event.target.name;
+
+    setChecked(
+      checked.includes(value) ? checked.filter((c) => c !== value) : [value]
+    );
   };
 
-  const handleChange1 = (event) => {
-
-  };
+  // const handleChange1 = (event) => {};
 
   return (
     <>
-  {props.addresses.length > 0
-            ? 
-        
-  props.addresses.map((address) => (
-              
-              
-    <Accordion>
-    <AccordionSummary
-      expandIcon={<ExpandMoreIcon />}
-      aria-label="Expand"
-      aria-controls="additional-actions2-content"
-      id="additional-actions2-header"
-    >
-      <FormControlLabel
-        aria-label="Acknowledge"
-        onClick={(event) => event.stopPropagation()}
-        onFocus={(event) => event.stopPropagation()}
-        control={<Checkbox onChange={e => handleCheck(e)} checked ={checked.includes(String(address.id))} name={address.id}/>}
-        label={address.name}
-      />
-    </AccordionSummary>
-    <AccordionDetails>
-      {/* <Typography color="textSecondary"> */}
-      <Grid item xs={12} sm={6}>
-      <Typography color="textSecondary">{address.name}</Typography>
-          <Typography color="textSecondary">{address.address1}, {address.address2},</Typography>
-          <Typography color="textSecondary">{address.city},{address.state}</Typography>
-          <Typography color="textSecondary">{address.postal}</Typography>
-      {/* </Typography> */}
-      </Grid>
-    </AccordionDetails>
-  </Accordion>    
-              
-               ))
-             
-             : <h4 class="Product_Text"> No Saved Address</h4> 
-       }
-      
-  
-
-      </>
-  )
-}
+      {props.addresses.length > 0 ? (
+        props.addresses.map((address) => (
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-label="Expand"
+              aria-controls="additional-actions2-content"
+              id="additional-actions2-header"
+            >
+              <FormControlLabel
+                aria-label="Acknowledge"
+                onClick={(event) => event.stopPropagation()}
+                onFocus={(event) => event.stopPropagation()}
+                control={
+                  <Checkbox
+                    onChange={(e) => handleCheck(e)}
+                    checked={checked.includes(String(address.id))}
+                    name={address.id}
+                  />
+                }
+                label={address.name}
+              />
+            </AccordionSummary>
+            <AccordionDetails>
+              {/* <Typography color="textSecondary"> */}
+              <Grid item xs={12} sm={6}>
+                <Typography color="textSecondary">{address.name}</Typography>
+                <Typography color="textSecondary">
+                  {address.address1}, {address.address2},
+                </Typography>
+                <Typography color="textSecondary">
+                  {address.city},{address.state}
+                </Typography>
+                <Typography color="textSecondary">{address.postal}</Typography>
+                {/* </Typography> */}
+              </Grid>
+            </AccordionDetails>
+          </Accordion>
+        ))
+      ) : (
+        <h4 class="Product_Text"> No Saved Address</h4>
+      )}
+    </>
+  );
+};
 
 const Checkout = () => {
-
   const [mobile, setMobile] = useState('');
   const [name, setName] = useState('');
   const [addressline1, setAddressline1] = useState('');
@@ -246,7 +230,7 @@ const Checkout = () => {
   const [error, setError] = useState('');
   const [addressId, setAddressId] = useState('');
   const address_create_url = `${API_URL}api/address/`;
-  
+
   const [addresses, setAddresses] = useState([]);
   const [createAddress, setCreateAddress] = useState(false);
   const [checked, setChecked] = useState([]);
@@ -257,167 +241,175 @@ const Checkout = () => {
     if (reason === 'clickaway') {
       return;
     }
-  
+
     setOpen(false);
   };
-
 
   useEffect(() => {
     axios.get(address_create_url).then((res) => {
       // return dispatch(fetchProduct(res.data.results));
       setAddresses(res.data.results);
-      console.log(res)
-      if(res.data.results.length > 0){
+      console.log(res);
+      if (res.data.results.length > 0) {
         setCreateAddress(false);
       }
       //setAddressId(res.data.results[0].id)
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch]);
 
   function getStepContent(step) {
     switch (step) {
-      case 0:{
-        
-        if(createAddress){
-          console.log("wss")
-        var address = {
-          mobile : mobile,
-          setMobile : setMobile,
-          mobileerror : mobileerror,
-          setMobileerror : setMobileerror,
-          name : name,
-          setName : setName,
-          addressline1 : addressline1,
-          setAddressline1 : setAddressline1,
-          addressline2 : addressline2,
-          setAddressline2 : setAddressline2,
-          city : city,
-          setCity : setCity,
-          state : state,
-          setState : setState,
-          pincode : pincode,
-          setPincode : setPincode,
+      case 0: {
+        if (createAddress) {
+          console.log('wss');
+          var address = {
+            mobile: mobile,
+            setMobile: setMobile,
+            mobileerror: mobileerror,
+            setMobileerror: setMobileerror,
+            name: name,
+            setName: setName,
+            addressline1: addressline1,
+            setAddressline1: setAddressline1,
+            addressline2: addressline2,
+            setAddressline2: setAddressline2,
+            city: city,
+            setCity: setCity,
+            state: state,
+            setState: setState,
+            pincode: pincode,
+            setPincode: setPincode,
+          };
+          return <AddressForm address={address} />;
         }
-        return <AddressForm address={address} />;
-         
-        }
-        return <Addresses addresses={addresses} checked={checked} setChecked={setChecked}/>;
-        
+        return (
+          <Addresses
+            addresses={addresses}
+            checked={checked}
+            setChecked={setChecked}
+          />
+        );
       }
-        
-        // 
-        
-      case 1:        
-      return <Cartpage />  
+
+      //
+
+      case 1:
+        return <Cartpage />;
       case 2:
-        var items= []
+        var items = [];
         console.log(cartItems);
-        cartItems.map((prod) =>(
-          items.push({ "quantity": prod.quantity,
-          "product": prod.id,
-          "price": prod.price_id})
-        ))
+        cartItems.map((prod) =>
+          items.push({
+            quantity: prod.quantity,
+            product: prod.id,
+            price: prod.price_id,
+          })
+        );
         var order = {
-          "address": addressId,
-          "total_price": 1000,
-          "payment_id": "",
-          "extra": "",
-          "order_status": "Processing",
-          "transaction_status": "Waiting",
-          "items": items
-          }
-        
-         return <PaymentForm order={order}/>;
+          address: addressId,
+          total_price: 1000,
+          payment_id: '',
+          extra: '',
+          order_status: 'Processing',
+          transaction_status: 'Waiting',
+          items: items,
+        };
+
+        return <PaymentForm order={order} />;
       default:
         console.log(activeStep);
         throw new Error('Unknown step');
-        // return <Review />;    
+      // return <Review />;
     }
   }
-  
 
   const handleNext = () => {
-    
-    if (activeStep == 0){
+    if (activeStep === 0) {
       console.log(checked);
-    if(!createAddress){
-      if(checked.length <1 ){
-        setError("Please select one address");
-        setOpen(true);
-        return;
-      }
-      else{
-        setAddressId(checked[0])
-      }
-  
-    }
-    else{
-      if(name.length == 0 ||  mobile.length !=13 || addressline1.length == 0 || city.length == 0 || state.length == 0 || pincode.length == 0)
-    {
-      setError("Please fill all the mandatory values");
+      if (!createAddress) {
+        if (checked.length < 1) {
+          setError('Please select one address');
           setOpen(true);
           return;
-    }
-      
-    
-      console.log(addressId)
-      if(addressId){
-        axios.put(`${address_create_url}${addressId}/`, {        
-          "phone": mobile.substring(3),        
-          "name": name,
-          "address1" : addressline1,          
-          "address2" : addressline2,          
-          "city" : city,          
-          "state" : state,          
-          "postal" : pincode, 
-          "country" : 'India',
-          "id" : addressId         
-          })
-          .then((response) => {
-            //dispatch(loginUser(response.data));
-            console.log(response);
-            setAddressId(response.data.id)
-          }, (error) => {
-            console.log(error);
-            setError("Unable to update address");
+        } else {
+          setAddressId(checked[0]);
+        }
+      } else {
+        if (
+          name.length === 0 ||
+          mobile.length !== 13 ||
+          addressline1.length === 0 ||
+          city.length === 0 ||
+          state.length === 0 ||
+          pincode.length === 0
+        ) {
+          setError('Please fill all the mandatory values');
           setOpen(true);
           return;
-          });
-      }
-      else{
-        axios.post(address_create_url, {        
-          "phone": mobile.substring(3),        
-          "name": name,
-          "address1" : addressline1,          
-          "address2" : addressline2,          
-          "city" : city,          
-          "state" : state,          
-          "postal" : pincode, 
-          "country" : 'India',
-          "id" : addressId         
-          })
-          .then((response) => {
-            //dispatch(loginUser(response.data));
-            console.log(response);
-            setAddressId(response.data.id)
-          }, (error) => {
-            console.log(error);
-            setError("Unable to update address");
-          setOpen(true);
-          return;
-          });
+        }
+
+        console.log(addressId);
+        if (addressId) {
+          axios
+            .put(`${address_create_url}${addressId}/`, {
+              phone: mobile.substring(3),
+              name: name,
+              address1: addressline1,
+              address2: addressline2,
+              city: city,
+              state: state,
+              postal: pincode,
+              country: 'India',
+              id: addressId,
+            })
+            .then(
+              (response) => {
+                //dispatch(loginUser(response.data));
+                console.log(response);
+                setAddressId(response.data.id);
+              },
+              (error) => {
+                console.log(error);
+                setError('Unable to update address');
+                setOpen(true);
+                return;
+              }
+            );
+        } else {
+          axios
+            .post(address_create_url, {
+              phone: mobile.substring(3),
+              name: name,
+              address1: addressline1,
+              address2: addressline2,
+              city: city,
+              state: state,
+              postal: pincode,
+              country: 'India',
+              id: addressId,
+            })
+            .then(
+              (response) => {
+                //dispatch(loginUser(response.data));
+                console.log(response);
+                setAddressId(response.data.id);
+              },
+              (error) => {
+                console.log(error);
+                setError('Unable to update address');
+                setOpen(true);
+                return;
+              }
+            );
+        }
       }
     }
-    
-    
-      
-    }
-    if(activeStep!=2)
-      setActiveStep(activeStep + 1);
+    if (activeStep !== 2) setActiveStep(activeStep + 1);
   };
 
   const handleBack = () => {
-    if(createAddress){
+    if (createAddress) {
       setCreateAddress(false);
       return;
     }
@@ -429,8 +421,6 @@ const Checkout = () => {
   //     .then((res) => setProduct(res.data));
   // }, [id]);
 
-
-  
   return (
     <div>
       <main className={classes.layout}>
@@ -438,7 +428,12 @@ const Checkout = () => {
           <Typography component="h1" variant="h4" align="center">
             Checkout
           </Typography>
-          <Stepper activeStep={activeStep} className={classes.stepper}>
+          <Stepper
+            activeStep={activeStep}
+            sx={{
+              padding: (theme) => theme.spacing(3, 0, 5),
+            }}
+          >
             {steps.map((label) => (
               <Step key={label}>
                 <StepLabel>{label}</StepLabel>
@@ -452,15 +447,16 @@ const Checkout = () => {
                   Thank you for your order.
                 </Typography>
                 <Typography variant="subtitle1">
-                  Your order number is #2001539. We have emailed your order confirmation, and will
-                  send you an update when your order has shipped.
+                  Your order number is #2001539. We have emailed your order
+                  confirmation, and will send you an update when your order has
+                  shipped.
                 </Typography>
               </React.Fragment>
             ) : (
               <React.Fragment>
                 {getStepContent(activeStep)}
 
-                {activeStep == 0 && !createAddress  && (
+                {activeStep === 0 && !createAddress && (
                   <Button
                     variant="contained"
                     color="primary"
@@ -468,24 +464,34 @@ const Checkout = () => {
                     className={classes.button}
                   >
                     Add New Address
-                  </Button>)
-                } 
+                  </Button>
+                )}
                 <div className={classes.buttons}>
-                  {activeStep !== 0 || createAddress &&(
-                    <Button onClick={handleBack} className={classes.button}>
-                      Back
+                  {activeStep !== 0 ||
+                    (createAddress && (
+                      <Button
+                        onClick={handleBack}
+                        sx={{
+                          marginTop: 3,
+                          marginLeft: 1,
+                        }}
+                      >
+                        Back
+                      </Button>
+                    ))}
+                  {activeStep !== steps.length - 1 && (
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleNext}
+                      sx={{
+                        marginTop: 3,
+                        marginLeft: 1,
+                      }}
+                    >
+                      {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
                     </Button>
                   )}
-                  {activeStep != steps.length - 1 &&(
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleNext}
-                    className={classes.button}
-                  >
-                    {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-                  </Button>)
-              }
                 </div>
               </React.Fragment>
             )}
@@ -493,7 +499,12 @@ const Checkout = () => {
         </Paper>
         {/* <Copyright /> */}
       </main>
-      <Snackbar open={open} autoHideDuration={6000}  anchorOrigin={{vertical:'top',horizontal:'right'}}onClose={handleClose}>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        onClose={handleClose}
+      >
         <Alert severity="error" onClose={handleClose}>
           {error}
         </Alert>
