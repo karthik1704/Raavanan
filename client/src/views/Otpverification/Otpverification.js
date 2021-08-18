994146be6dc10e2e05f8e71206d224ff097ec94c
Karthik A <karthikthee7@gmail.com>
2021-08-18 21:34:02 +0530

    updated to v5 styles

--- client/src/views/Otpverification/Otpverification.js
+++ client/src/views/Otpverification/Otpverification.js
@@ -1,18 +1,15 @@
-import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import makeStyles from '@material-ui/styles/makeStyles';
+import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import Header from "../../components/Header/Header";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Link from '@material-ui/core/Link';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+import Header from '../../components/Header/Header';
 
 function Copyright() {
   return (
@@ -27,86 +24,107 @@ function Copyright() {
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor:"#43a047"
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  padding_bottom:
-  {
-      paddingBottom:10,
-  }
-
-}));
+// const useStyles = makeStyles((theme) => ({
+//   paper: {
+//     marginTop: theme.spacing(8),
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//   },
+//   avatar: {
+//     margin: theme.spacing(1),
+//     backgroundColor: '#43a047',
+//   },
+//   form: {
+//     width: '100%', // Fix IE 11 issue.
+//     marginTop: theme.spacing(1),
+//   },
+//   submit: {
+//     margin: theme.spacing(3, 0, 2),
+//   },
+//   padding_bottom: {
+//     paddingBottom: 10,
+//   },
+// }));
 
 export default function Otpverification() {
-  const classes = useStyles();
-
   return (
-      <>
-       <Header title="Create Account"  subtitle="Home"/>
-   
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
+    <>
+      <Header title="Create Account" subtitle="Home" />
 
-     
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5" className={classes.padding_bottom}>
-       Otp Verification
-        </Typography>
-        <Typography component="p" variant="p">
-       Enter the correct 6 digits number
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Otp"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            className="Register_text"
-            inputProps={{
-              maxlength :6,
-              autocomplete : false
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar
+            sx={{
+              margin: 1,
+              backgroundColor: '#43a047',
             }}
-          />
-          
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
           >
-          Submit
-          </Button>
-        
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography
+            component="h1"
+            variant="h5"
+            sx={{
+              pb: '10px',
+            }}
+          >
+            Otp Verification
+          </Typography>
+          <Typography component="p" variant="p">
+            Enter the correct 6 digits number
+          </Typography>
+          <Box
+            component="form"
+            sx={{
+              width: '100%', // Fix IE 11 issue.
+              marginTop: 1,
+            }}
+            noValidate
+          >
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Otp"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              className="Register_text"
+              inputProps={{
+                maxlength: 6,
+                autocomplete: false,
+              }}
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              sx={{
+                m: (theme) => theme.spacing(3, 0, 2),
+              }}
+            >
+              Submit
+            </Button>
+          </Box>
+        </Box>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
     </>
   );
 }
