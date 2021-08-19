a41c315ceaa1d6f067bc7f4e8d14d678ba1aa502
Karthik A <karthikthee7@gmail.com>
2021-08-19 21:53:22 +0530

    Removed imports

--- client/src/components/Footer/style.js
+++ client/src/components/Footer/style.js
@@ -1,11 +1,11 @@
-import makeStyles from '@material-ui/styles/makeStyles';
+// import makeStyles from '@material-ui/styles/makeStyles';
 
-// eslint-disable-next-line import/prefer-default-export
-export const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: '100vw',
-    [theme.breakpoints.down('md')]: {
-      display: 'none',
-    },
-  },
-}));
+// // eslint-disable-next-line import/prefer-default-export
+// export const useStyles = makeStyles((theme) => ({
+//   root: {
+//     maxWidth: '100vw',
+//     [theme.breakpoints.down('md')]: {
+//       display: 'none',
+//     },
+//   },
+// }));
