2b61b3d2b8a221626f0d5243db63ed341cd83759
Karthik A <karthikthee7@gmail.com>
2021-10-02 20:05:21 +0530

    untested interceptors

--- client/src/helper/axiosInterceptor.js
+++ client/src/helper/axiosInterceptor.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+import { logoutUser, refreshAccessToken } from '../features/auth/authSlice';
+
+import { API_URL } from '../CONSTANTS';
+
 let headers = {};
 
 const myAxios = axios.create({
@@ -14,6 +18,35 @@ myAxios.interceptors.request.use(
   },
   (error) => {
     console.error('Axios Error -> ', JSON.stringify(error));
+    return Promise.reject(error);
+  }
+);
+
+myAxios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      axios
+        .post(`${API_URL}api/auth/token/refresh/ `, {
+          refresh: localStorage.getItem('refresh_token'),
+        })
+        .then((res) => {
+          const access_token = res.data.access;
+          refreshAccessToken(res.data);
+          axios.defaults.headers.common['Authorization'] =
+            'Bearer ' + access_token;
+          originalRequest.headers['Authorization'] = 'Bearer ' + access_token;
+          return myAxios(originalRequest);
+        })
+        .catch((error) => {
+          logoutUser();
+          return Promise.reject(error);
+        });
+    }
     return Promise.reject(error);
   }
 );
