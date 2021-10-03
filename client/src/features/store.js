dc8f1e48f7c08b702c66a884bb5fee2e3b93d766
Karthik A <karthikthee7@gmail.com>
2021-10-03 21:10:14 +0530

    add auth, authApi and theme reducers to store

--- client/src/features/store.js
+++ client/src/features/store.js
@@ -1,8 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+// Slice Reducers imports
+import authReducer from './auth/authSlice';
+import themeReducer from './theme/themeSlice';
+
+// API Reducers imports
+import { authApi } from './auth/authApi';
+
 const store = configureStore({
-  reducer: {},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+  reducer: {
+    //Add Slice Reducers here
+    auth: authReducer,
+    theme: themeReducer,
+
+    // Add API Reducers here
+    [authApi.reducerPath]: authApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    // Add Api reducer middleware here
+    getDefaultMiddleware().concat(authApi.middleware),
 });
 
 export default store;
