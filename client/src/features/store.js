4f811912fff24aa467eb25a2c6ca2d0e46225d1e
Karthik A <karthikthee7@gmail.com>
2021-10-08 20:10:41 +0530

    add logger

--- client/src/features/store.js
+++ client/src/features/store.js
@@ -3,24 +3,38 @@ import { configureStore } from '@reduxjs/toolkit';
 // Slice Reducers imports
 import authReducer from './auth/authSlice';
 import appDrawerReducer from './appDrawer/appDrawerSlice';
+import loaderReducer from './loader/loaderSlice';
 import themeReducer from './theme/themeSlice';
 
 // API Reducers imports
 import { authApi } from './auth/authApi';
+import { productApi } from './product/productApi';
+
+// in Future remove will be removed
+import cartReducer from '../data/reducers/cartReducer';
+
+import logger from 'redux-logger';
 
 const store = configureStore({
   reducer: {
     //Add Slice Reducers here
     auth: authReducer,
     appDrawer: appDrawerReducer,
+    loader: loaderReducer,
     theme: themeReducer,
+    cart: cartReducer,
 
     // Add API Reducers here
     [authApi.reducerPath]: authApi.reducer,
+    [productApi.reducerPath]: productApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     // Add Api reducer middleware here
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(
+      logger,
+      authApi.middleware,
+      productApi.middleware
+    ),
 });
 
 export default store;
