94cccd24e43b3ad278e0fc655445bf8658c12e93
Karthik A <karthikthee7@gmail.com>
2021-10-08 20:41:42 +0530

    update theme with new theme folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
@@ -7,103 +7,89 @@ import {
   ThemeProvider,
   StyledEngineProvider,
 } from '@mui/material/styles';
-import { green } from '@mui/material/colors';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes/Routes';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import useDarkTheme from './hooks/useDarkTheme';
 
 import Loader from './components/Loader/Loader';
 import Navbar from './components/navbar/Navbar';
 import NewFooter from './components/NewFooter/NewFooter';
-// import { dark, light } from './theme';
 
 import ReactGA from 'react-ga';
 import { Helmet } from 'react-helmet';
 import { store } from './data/store';
 import Logo from './asserts/raavanan logo.png';
 import axios from 'axios';
-import { toggleAppLoading } from './data/actions/appAction';
-import { logoutUser } from './data/actions/loginActions';
+// import { toggleAppLoading } from './data/actions/appAction';
+// import { logoutUser } from './data/actions/loginActions';
+
+import { toggleLoader } from './features/loader/loaderSlice';
+import { logoutUser } from './features/auth/authSlice';
+
+import { getThemeByName } from './theme';
 
 function App() {
-  const [theme] = useDarkTheme();
-  const { loading } = useSelector((state) => state.appUi);
-  //const { dispatch } = useDispatch();
-  const { dispatch } = store;
+  const [mode] = useDarkTheme();
+  const { topLoader } = useSelector((state) => state.loader);
+  const { dispatch } = useDispatch();
 
-  const darkTheme = createTheme({
-    palette: {
-      mode: 'dark',
-    },
-  });
+  const theme = useMemo(() => createTheme(getThemeByName(mode)), [mode]);
 
-  const lightTheme = createTheme({
-    palette: {
-      primary: {
-        main: green[600],
-        // main: '#8bc34a'
-      },
-    },
-  });
+  // const { dispatch } = store;
 
-  useEffect(() => {
-    // const self = this;
+  // useEffect(() => {
 
-    //console.log(self.props);
-    axios.interceptors.request.use(
-      function (config) {
-        // spinning start to show
-        //self.props.loading(true)
-        //dispatch(appUi(true))
+  //   axios.interceptors.request.use(
+  //     function (config) {
+  //       // spinning start to show
+  //       dispatch(toggleLoader(true));
+  //       //useTopLoader(true)
+  //       return config;
+  //     },
+  //     function (error) {
+  //       return Promise.reject(error);
+  //     }
+  //   );
 
-        dispatch(toggleAppLoading(true));
-        //useTopLoader(true)
-        return config;
-      },
-      function (error) {
-        return Promise.reject(error);
-      }
-    );
+  //   axios.interceptors.response.use(
+  //     function (response) {
+  //       // spinning hide
+  //       // self.props.loading(false)
 
-    axios.interceptors.response.use(
-      function (response) {
-        // spinning hide
-        // self.props.loading(false)
+  //       dispatch(toggleLoader(false));
+  //       return response;
+  //     },
+  //     function (error) {
+  //       // if(!error.response)
+  //       // return;
+  //       if (error.response.status === 401) {
+  //         localStorage.setItem('app_token', '');
+  //         axios.defaults.headers.common['Authorization'] = '';
+  //         dispatch(logoutUser(''));
+  //         // Router.push('/');
+  //         window.location.reload();
+  //         return Promise.reject(error);
+  //       }
+  //       //   if(error.response.data.code == "token_not_valid"){
 
-        dispatch(toggleAppLoading(false));
-        return response;
-      },
-      function (error) {
-        // if(!error.response)
-        // return;
-        if (error.response.status === 401) {
-          localStorage.setItem('app_token', '');
-          axios.defaults.headers.common['Authorization'] = '';
-          dispatch(logoutUser(''));
-          // Router.push('/');
-          window.location.reload();
-          return Promise.reject(error);
-        }
-        //   if(error.response.data.code == "token_not_valid"){
+  //       //   localStorage.setItem("app_token", '');
+  //       //   axios.defaults.headers.common['Authorization'] = '';
+  //       //   //Promise.reject(error);
+  //       // //   dispatch(logoutUser(''));
+  //       // // <Redirect to="/" />
 
-        //   localStorage.setItem("app_token", '');
-        //   axios.defaults.headers.common['Authorization'] = '';
-        //   //Promise.reject(error);
-        // //   dispatch(logoutUser(''));
-        // // <Redirect to="/" />
+  //       //   //window.location.href = '/login'
+  //       //   return Promise.reject(error);
 
-        //   //window.location.href = '/login'
-        //   return Promise.reject(error);
-
-        // }
-        return Promise.reject(error);
-      }
-    );
-  }, []);
+  //       // }
+  //       return Promise.reject(error);
+  //     }
+  //   );
+  // }, []);
 
   useEffect(() => {
     ReactGA.initialize('G-LH9KB8TXPW');
@@ -126,10 +112,10 @@ function App() {
         />
       </Helmet>
       <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        <ThemeProvider theme={theme}>
           <Router>
             <Paper>
-              {loading && <Loader />}
+              {topLoader && <Loader />}
               <Navbar />
               <Box
                 sx={{
