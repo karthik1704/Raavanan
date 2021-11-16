a456c22750dff9e7804cfbbe9b0fd2dbe6ab73fb
Karthi A <karthikthee7@gmail.com>
2021-11-16 08:46:27 +0530

    update orders with rtk query

--- client/src/views/Orders/Orders.js
+++ client/src/views/Orders/Orders.js
@@ -1,5 +1,3 @@
-import { useState, useEffect } from 'react';
-
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -7,16 +5,14 @@ import CardActions from '@mui/material/CardActions';
 import CardHeader from '@mui/material/CardHeader';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { useDispatch } from 'react-redux';
-// import makeStyles from '@mui/styles/makeStyles';
+
 import { styled } from '@mui/material/styles';
 
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import { useGetOrderQuery } from '../../features/orders/orderApi';
 
 import './Orders.css';
-
-import { API_URL } from '../../CONSTANTS';
 
 const Div = styled('div')(({ theme }) => ({
   margin: theme.spacing(2),
@@ -24,6 +20,70 @@ const Div = styled('div')(({ theme }) => ({
     margin: '16px 0',
   },
 }));
+
+const Orders = () => {
+  const { data: orders, isLoading, isFetching, error } = useGetOrderQuery();
+
+  return (
+    <Div>
+      <Grid container spacing={3}>
+        {orders ? (
+          orders.map((order) => (
+            <Grid item xs={12} sm={4} key={order.id}>
+              <Card>
+                <CardHeader
+                  sx={{
+                    padding: 2,
+                    textAlign: 'center',
+                    background: '#43a047',
+                    color: 'white',
+                    fontSize: '14px',
+                  }}
+                  title={'#RAAV' + order.id}
+                  subheader={new Date(order.created_at).toDateString()}
+                ></CardHeader>
+
+                <CardContent>
+                  <Typography sx={{ textAlign: 'center' }} gutterBottom>
+                    <strong>No of Items Ordered :</strong> {order.items.length}
+                  </Typography>
+
+                  <Typography sx={{ textAlign: 'center' }} gutterBottom>
+                    <strong>Total Price:</strong> ₹ {order.total_price}
+                  </Typography>
+                  <Typography sx={{ textAlign: 'center' }} gutterBottom>
+                    <strong>Status :</strong> {order.order_status}
+                  </Typography>
+                </CardContent>
+                <CardActions sx={{ display: 'flex', justifyContent: 'center' }}>
+                  <Button
+                    size="small"
+                    sx={{
+                      padding: 1,
+                      textAlign: 'center',
+                      background: '#43a047',
+                      color: 'white',
+                      display: 'flex',
+                      justifyContent: 'center',
+                    }}
+                    component={Link}
+                    to={`/RAAV${order.id}`}
+                  >
+                    View Details
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))
+        ) : (
+          <h4 className="Product_Text"> You have not ordered anything</h4>
+        )}
+      </Grid>
+    </Div>
+  );
+};
+
+export default Orders;
 
 // const useStyles = makeStyles((theme) => ({
 //   root: {
@@ -66,84 +126,3 @@ const Div = styled('div')(({ theme }) => ({
 //     justifyContent: 'center',
 //   },
 // }));
-
-const Orders = () => {
-  const dispatch = useDispatch();
-  const [orders, setOrders] = useState([]);
-  const orders_url = `${API_URL}orders/`;
-  let navigate = useNavigate();
-  console.log(orders);
-  useEffect(() => {
-    axios.get(orders_url).then((res) => {
-      // return dispatch(fetchProduct(res.data.results));
-      console.log(res);
-      setOrders(res.data);
-
-      //setAddressId(res.data.results[0].id)
-    });
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dispatch]);
-
-  const callOrderDetail = (order_code) => {
-    navigate('/orderdetail/RAAV' + order_code + '/');
-  };
-  return (
-    <Div>
-      <Grid container spacing={3}>
-        {orders.length > 0 ? (
-          orders.map((order) => (
-            <Grid item xs={12} sm={4} key={order.id}>
-              <Card>
-                <CardHeader
-                  sx={{
-                    padding: 2,
-                    textAlign: 'center',
-                    background: '#43a047',
-                    color: 'white',
-                    fontSize: '14px',
-                  }}
-                  title={'#RAAV' + order.id}
-                  subheader={new Date(order.created_at).toDateString()}
-                ></CardHeader>
-
-                <CardContent>
-                  <Typography sx={{ textAlign: 'center' }} gutterBottom>
-                    <strong>No of Items Ordered :</strong> {order.items.length}
-                  </Typography>
-
-                  <Typography sx={{ textAlign: 'center' }} gutterBottom>
-                    <strong>Total Price:</strong> ₹ {order.total_price}
-                  </Typography>
-                  <Typography sx={{ textAlign: 'center' }} gutterBottom>
-                    <strong>Status :</strong> {order.order_status}
-                  </Typography>
-                </CardContent>
-                <CardActions sx={{ display: 'flex', justifyContent: 'center' }}>
-                  <Button
-                    size="small"
-                    sx={{
-                      padding: 1,
-                      textAlign: 'center',
-                      background: '#43a047',
-                      color: 'white',
-                      display: 'flex',
-                      justifyContent: 'center',
-                    }}
-                    onClick={() => callOrderDetail(order.id)}
-                  >
-                    View Details
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))
-        ) : (
-          <h4 className="Product_Text"> You have not ordered anything</h4>
-        )}
-      </Grid>
-    </Div>
-  );
-};
-
-export default Orders;
