d6b93b8dcd774379928903db397ecebd5d2364de
Karthik A <karthikthee7@gmail.com>
2021-12-02 21:19:49 +0530

    add addressApi to store

--- client/src/features/store.js
+++ client/src/features/store.js
@@ -7,14 +7,15 @@ import loaderReducer from './loader/loaderSlice';
 import themeReducer from './theme/themeSlice';
 
 // API Reducers imports
+import { addressApi } from './address/addressApi';
 import { authApi } from './auth/authApi';
+import { orderApi } from './orders/orderApi';
 import { productApi } from './product/productApi';
 
 // in Future remove will be removed
 import cartReducer from '../data/reducers/cartReducer';
 
 import logger from 'redux-logger';
-import { orderApi } from './orders/orderApi';
 
 const store = configureStore({
   reducer: {
@@ -29,6 +30,7 @@ const store = configureStore({
     [authApi.reducerPath]: authApi.reducer,
     [productApi.reducerPath]: productApi.reducer,
     [orderApi.reducerPath]: orderApi.reducer,
+    [addressApi.reducerPath]: addressApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     // Add Api reducer middleware here
@@ -36,7 +38,8 @@ const store = configureStore({
       logger,
       authApi.middleware,
       productApi.middleware,
-      orderApi.middleware
+      orderApi.middleware,
+      addressApi.middleware
     ),
 });
 
