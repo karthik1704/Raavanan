6f0196e865a662ee91def6a98aae73a69e9e056c
Karthik A <karthikthee7@gmail.com>
2021-12-04 12:33:31 +0530

    pageNotFound design updated

--- client/src/views/PageNotFound/PageNotFound.jsx
+++ client/src/views/PageNotFound/PageNotFound.jsx
@@ -2,16 +2,19 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 import { Link } from 'react-router-dom';
-
-import { Styled } from '@mui/material/styles';
-
 import { Helmet } from 'react-helmet';
 
-const CenterBox = Styled(Box)((theme) => ({
+import { styled } from '@mui/material/styles';
+
+const CenterBox = styled(Box)(({ theme }) => ({
   display: 'flex',
+  flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
+  height: '100vh',
 }));
 
 const PageNotFound = () => {
@@ -22,10 +25,26 @@ const PageNotFound = () => {
       </Helmet>
       <CenterBox>
         <Typography variant="h2" component="div">
-          Page 404{' '}
+          Oops! | Page 404{' '}
         </Typography>
-        <Typography variant="body1">Page Not Found </Typography>
-        <Button variant="outline" color="primary" component={Link} to="..">
+        <Typography
+          variant="body1"
+          sx={{
+            mt: 2,
+          }}
+        >
+          We can&rsquo;t find a Page you looking for :({' '}
+        </Typography>
+        <Button
+          sx={{
+            mt: 2,
+          }}
+          variant="outlined"
+          color="primary"
+          startIcon={<ArrowBackIcon />}
+          component={Link}
+          to=".."
+        >
           Go Back
         </Button>
       </CenterBox>
